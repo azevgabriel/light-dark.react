25d0c6aeabe9e9cd06e58fa3fe813b1f346be372
Gabriel Azevedo <azevgabriel@gmail.com>
2021-02-27 15:47:45 -0300

    dark-light toggle

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import {ThemeProvider, DefaultTheme} from 'styled-components';
+
+import light from './styles/themes/light';
+import dark from './styles/themes/dark';
+
+import GlobalStyle from './styles/global';
+
+import Header from './components/Header';
+
+import usePeristedState from './utils/usePersistedState';
+
+const App = () => {
+  const [theme,setTheme] = usePeristedState<DefaultTheme>('theme',light);
+
+  const toggleTheme = () => {
+    setTheme(theme.title === 'light' ? dark : light);
+  };
+
+  return (
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <GlobalStyle/>
+        <Header toggleTheme={toggleTheme}/>
+      </div>
+    </ThemeProvider>
+  );
+}
+
+export default App;
